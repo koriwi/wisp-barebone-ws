ab5ad88166bac20dbb0dabf048626cdcd6af69e3
koriwi <kilian2798@gmail.com>
2018-09-30 12:34:39 +0200

    lel

---
+++ index.js
@@ -0,0 +1,62 @@
+const WebSocket = require('ws')
+const http = require('http')
+
+const server = new http.createServer();
+const pingServer = new http.createServer();
+const port = 8080
+server.listen(port)
+pingServer.listen(port + 1)
+const wss = new WebSocket.Server({ server });
+const pws = new WebSocket.Server({ server: pingServer })
+
+console.log('listening on port: ' + port);
+let clients = []
+
+pws.on('connection', (connection) => {
+	console.log('pinger connected')
+	connection.on('message', (message) => {
+		setTimeout(() => {
+			try {
+				connection.send(message)
+			} catch (e) {
+				connection.close()
+			}
+		}, Math.random() * 1)
+	})
+})
+
+wss.on('connection', (connection, newCLient) => {
+	console.log('new client')
+	console.log(newCLient.headers['sec-websocket-key'])
+
+	clients.push({ id: newCLient.headers['sec-websocket-key'], connection })
+	//tell everyone about the new guy
+	clients/*.filter(con => con != connection)*/.forEach(client => {
+		connection.send(JSON.stringify({
+			type: 'newClient',
+			payload: client.id
+		}))
+		if (client.connection.readyState === WebSocket.OPEN)
+			client.connection.send(JSON.stringify({
+				type: 'newClient',
+				payload: newCLient.headers['sec-websocket-key']
+			}))
+	})
+
+	//broadcast
+	connection.on('message', (audio) => {
+		clients/*.filter(con => con != connection)*/.forEach(client => {
+			if (client.connection.readyState === WebSocket.OPEN)
+				client.connection.send(JSON.stringify({
+					type: 'audio',
+					id: newCLient.headers['sec-websocket-key'],
+					payload: audio
+				}))
+		})
+	});
+	connection.on('close', () =>  {
+		clients = clients.filter(con => {
+			return con.connection.readyState !== 3
+		})
+	})
+});
